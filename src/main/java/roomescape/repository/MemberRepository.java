a887117268c24001cacb5b7ee0874bd02cec6f77
psy3720 <qkrtmddyd12@naver.com>
2024-07-15 22:21:07 +0900

    refactor: 관리자 생성 수정

--- src/main/java/roomescape/repository/MemberRepository.java
+++ src/main/java/roomescape/repository/MemberRepository.java
@@ -2,11 +2,7 @@ package roomescape.repository;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
 import roomescape.domain.Member;
 
 public interface MemberRepository extends CrudRepository<Member, Long> {
@@ -14,12 +10,4 @@ public interface MemberRepository extends CrudRepository<Member, Long> {
     List<Member> findAll();
 
     Optional<Member> findByEmail(String email);
-
-    @Transactional
-    @Modifying
-    @Query(value= """
-        insert into member (id, email, name, password, role_id) 
-        VALUES (9999 ,'admin@email.com', '관리자', '$2a$10$HGgzwqW6INvWjAfhRSQwR.fbwnfvgDlz8BPeMAepj9BUNyJO3Eu.a', 2)
-    """, nativeQuery = true)
-    void createAdmin();
 }
--- src/test/java/roomescape/controller/DefaultTestBase.java
+++ src/test/java/roomescape/controller/DefaultTestBase.java
@@ -3,6 +3,9 @@ package roomescape.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import roomescape.domain.Member;
+import roomescape.domain.Role;
+import roomescape.domain.RoleType;
 import roomescape.repository.MemberRepository;
 import roomescape.util.DatabaseCleanup;
 
@@ -19,6 +22,16 @@ public class DefaultTestBase {
     void cleanUp() {
         dbDatabaseCleanup.execute();
 
-        memberRepository.createAdmin();
+        createAdmin();
+    }
+
+    private void createAdmin() {
+        Member admin = new Member(
+                9999L
+                , "관리자"
+                , "admin@email.com"
+                , "$2a$10$HGgzwqW6INvWjAfhRSQwR.fbwnfvgDlz8BPeMAepj9BUNyJO3Eu.a"
+                , new Role(2L, RoleType.ADMIN));
+        memberRepository.save(admin);
     }
 }
